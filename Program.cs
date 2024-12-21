a0c738721ae12644760924b5d1c3e7e7db62ce7d
Марина Чистякова <m2414143@edu.misis.ru>
2024-12-21 15:05:39 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -993,7 +993,34 @@ public class Program
     public int[,] Task_3_11(int[,] matrix)
     {
         // code here
+        int k = matrix.GetLength(0);
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i,j] == 0)
+                {
+                    k--;
+                    matrix[i, 0] = 0;
+                    break;
+                }
 
+            }
+        }
+        int[,] A = new int[k, matrix.GetLength(1)];
+        int n = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            if (matrix[i, 0] != 0)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    A[n, j] = matrix[i, j];
+                }
+                n++;
+            }
+        }
+        matrix = A;
         // end
 
         return matrix;
